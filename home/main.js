0506c0770ae51133a78952bb3211ef2edc87b9ec
John <142847092+John-2050@users.noreply.github.com>
2024-11-29 20:17:32 +0000

    Update main.js

--- home/main.js
+++ home/main.js
@@ -1,9 +1,1 @@
-jQuery(window).load(function() {
-            // will first fade out the loading animation
-        jQuery(".loader").fadeOut();
-            // will fade out the whole DIV that covers the website.
-        jQuery(".preloader").delay(1000).fadeOut("slow");
-    });
 
-	jQuery('#s').attr('placeholder','Type and hit enter...');
-	
